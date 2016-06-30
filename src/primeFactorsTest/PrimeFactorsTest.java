b98c7f39d2f0f87f8d2bfba4faae050cdf0478f6
AlenT <atrobej@outlook.com>
2016-06-30 12:06:34 +0200

    Initial commit

---
+++ src/primeFactorsTest/PrimeFactorsTest.java
@@ -0,0 +1,14 @@
+package primeFactorsTest;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PrimeFactorsTest {
+
+    @Test
+    public void doNothing(){
+        Assert.assertEquals("","");
+    }
+
+}
